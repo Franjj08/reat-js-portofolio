0048fdc8f58a376c97d6220a3e5d50212a38b346
Jiangjie Chen <franciscochen.cjj@gmail.com>
2024-08-16 20:06:12 -0300

    Navbar finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navabar from './Pages/Home/Navbar';
 import Home from './Pages/Home/Homescreen';
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <div>
+          <Navabar/>
+          <Routes>
+            <Route path='/' element={<Home/>}>
+            </Route>
+            <Route path='*' element={<div>404 Not Found</div>}>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
