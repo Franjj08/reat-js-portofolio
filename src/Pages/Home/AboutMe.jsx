586ab28e775627ee5c43edcf0e5fe9297af93749
Jiangjie Chen <franciscochen.cjj@gmail.com>
2024-08-18 23:20:59 -0300

    About Me section finished

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,17 @@
+export default function AboutMe(){
+    return(
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="about Me"/>
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">About</p>
+                    <h1 className="skills-section--heading">About Me</h1>
+                    <p className="hero--section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto perspiciatis eius maxime in quidem explicabo similique totam quod illo molestias laudantium sequi, sit sapiente minus nemo consequatur, id numquam? Eaque?</p>
+                    <p className="hero--section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto perspiciatis eius maxime in quidem explicabo similique totam quod illo molestias laudantium sequi, sit sapiente minus nemo consequatur, id numquam? Eaque?</p>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkill";
+import AboutMe from "../AboutMe";
 
 export default function Home(){
     return(
        <>
         <HeroSection/>
         <MySkills/>
+        <AboutMe/>
        </>
     );
 }
